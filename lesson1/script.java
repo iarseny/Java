a1deb7d9ec4d5a91ba30d1ffa3ba3b666959affa
iarseny <122807698+iarseny@users.noreply.github.com>
2023-05-30 10:29:26 +0000

    lesson

--- lesson1/script.java
+++ lesson1/script.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
 
+public class Main{
+    public static void main(String[] args) {
+        System.out.print("Привет!");
+        byte nums = 2;
+        int a = 12345;
+        long b = 999999999;
+        float num = 5.5f;
+        System.out.print(nums);
+        System.out.print("\n");
+        System.out.print(num);
+        char symbol = 'a';
+        boolean q = true;
+        String g = "\nПривет, братан!=" + q + "\n";
+        System.out.print(g);
+
+        Scanner num2 = new Scanner(System.in);
+        String i = num2.nextLine();
+        //num2.nextInt(), nextDouble
+        System.out.print("\n" + i);
+    }
+}
